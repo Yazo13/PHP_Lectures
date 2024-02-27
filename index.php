f9004167c9a1aaaf2f9006e45739ca36e71e5f2e
LAB321 Student10 <giorgi.kazishvili@gau.edu.ge>
2024-02-27 12:35:08 +0400

    commit1

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row Heading">
+            <div><h3>Questions</h3></div>
+            <div><h3>Answers</h3></div>
+            <div><h3>Grade</h3></div>
+        </div>
+        <div class="row">
+            <div>what is HTML?</div>
+            <input type="text" placeholder="answer">
+            <div>7</div>
+        </div>
+        <div class="row">
+            <div>what is HTML?</div>
+            <input type="text" placeholder="answer">
+            <div>7</div>
+        </div>
+
+        <div class="button"><button>Sent</button></div>
+    </div>
+    <?php 
+
+        
+
+    ?>
+</body>
+</html>
